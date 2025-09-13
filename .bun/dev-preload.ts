876b292ffefe3c43d80c36c95fa8eefe6c078bf5
shiraya-ma <mai-shirayama@outlook.jp>
2025-09-13 22:58:39 +0900

    :sparkles: replace with node-logger

--- .bun/dev-preload.ts
+++ .bun/dev-preload.ts
@@ -1,9 +1,10 @@
 'use strict';
 import { copyFileSync, existsSync, rmSync } from 'fs';
 import { resolve } from 'path';
-import { DefaultLogger, MaiLogLevels } from '../src';
+import { DefaultLogger, MaiLogLevels, NodeLogger } from '../src';
 
 const dLog = new DefaultLogger({ level: MaiLogLevels.trace });
+const nLog = new NodeLogger({ level: MaiLogLevels.trace });
 
 const rootdir = resolve(__dirname, '..');
 const srcIndexHTML = resolve(rootdir, '.bun', 'index.html');
@@ -11,11 +12,11 @@ const distIndexHTML = resolve(rootdir, 'index.html');
 
 if (existsSync(distIndexHTML)) {
   rmSync(distIndexHTML);
-  dLog.info(`Removed: ${distIndexHTML}`);
+  nLog.info(`Removed: ${distIndexHTML}`);
 }
 
 copyFileSync(srcIndexHTML, distIndexHTML);
-dLog.info(`Copied: ${srcIndexHTML} -> ${distIndexHTML}`);
+nLog.info(`Copied: ${srcIndexHTML} -> ${distIndexHTML}`);
 
 dLog.trace('this is default trace log');
 dLog.debug('this is default debug log');
@@ -23,4 +24,4 @@ dLog.info('this is default info log');
 dLog.warn('this is default warn log');
 dLog.error('this is default error log');
 
-dLog.info('Preload script is done. You can start development server with `vite dev`.');
+nLog.info('Preload script is done. You can start development server with `vite dev`.');
